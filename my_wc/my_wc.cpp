bea26e6decccc13c37a2aeb50e9bb61158d5fa27
bradyAdger12 <preston.adger@western.edu>
2019-02-04 09:31:24 -0700

    update hw03

--- my_wc/my_wc.cpp
+++ my_wc/my_wc.cpp
@@ -24,11 +24,14 @@ int main(int argc, char **argv) {
        int lc = 0; //line count for file
        long int cc = 0; //character count for file
        file = fopen(argv[i], "r");
+       if(file == NULL) {
+			 		perror("ERROR");
+					exit(1);
+			 } 
        //while loop that looks at every string in file
        while(fgets(buffer, 100, file) != NULL) {
            ptr = strtok(buffer, " "); //points to a first string in line
-	   lc++; //increment line count
-
+	         lc++; //increment line count
            //while loop that looks at every character on a line
            while(ptr != NULL) {
                 //strip off newlines
@@ -37,7 +40,7 @@ int main(int argc, char **argv) {
                    ptr[strip-1] = '\0';
                 }
                 cc+=strlen(ptr); //add characters from string to string count
- 		wc++; //add entire word to word count 
+ 	            	wc++; //add entire word to word count 
                 ptr = strtok(NULL, " ");    
            }
        }
@@ -46,10 +49,11 @@ int main(int argc, char **argv) {
            twc += wc;
            tlc += lc;
            tcc += cc;
-       }   
+       }
+       fclose(file);   
     }
     if(multFiles) {
       	 printf("twc: %d tlc: %d tcc: %ld\n", twc, tlc, tcc);
     }													
-    
+
 }
